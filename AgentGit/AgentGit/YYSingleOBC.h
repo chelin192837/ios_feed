49592fcb88cf73372901f6213839e20e5d0557fb
yujing <1181844910@qq.com>
2019-03-05 17:35:13 +0800

    提交…

---
+++ AgentGit/AgentGit/YYSingleOBC.h
@@ -0,0 +1,17 @@
+//
+//  YYSingleOBC.h
+//  AgentGit
+//
+//  Created by qsm on 2019/3/5.
+//  Copyright © 2019年 qsm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YYSingleOBC : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AgentGit/AgentGit/YYSingleOBC.m
@@ -0,0 +1,13 @@
+//
+//  YYSingleOBC.m
+//  AgentGit
+//
+//  Created by qsm on 2019/3/5.
+//  Copyright © 2019年 qsm. All rights reserved.
+//
+
+#import "YYSingleOBC.h"
+
+@implementation YYSingleOBC
+
+@end
